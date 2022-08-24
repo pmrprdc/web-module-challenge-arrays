8d461327613197732029e3f4bc1549b989be020c
Pamir Perdeci <pamirperdeci@gmail.com>
2022-08-24 11:50:49 -0400

    3 tests passing, solved task 2

--- index.js
+++ index.js
@@ -65,9 +65,15 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-  /*your code here*/
+function is31Flavors(array){
+  if (array.length === 31) {
+    return true;
+  } else {
+    return false;
+  }
  }
+
+
 
 
 
